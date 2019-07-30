c379c47213ec3186c915ccc87362fc503a9359c9
Kapil Garg <kapil.garg@letme.com>
2019-07-30 15:39:30 +0530

    first commit

---
+++ PaypalIntegration/Controllers/PaypalController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using BraintreeHttp;
+using PayPalCheckoutSdk.Core;
+using PayPalCheckoutSdk.Orders;
+
+namespace PaypalData.Controllers
+{
+    public class PaypalController : Controller
+    {
+        // GET: Paypal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async static Task<HttpResponse> GetOrder(string orderId, bool debug = false)
+            {
+            OrdersGetRequest request = new OrdersGetRequest(orderId);
+            //3. Call PayPal to get the transaction
+            var response = await client().Execute(request);
+            //4. Save the transaction in your database. Implement logic to save transaction to your database for future reference.
+            var result = response.Result<Order>();
+            Console.WriteLine("Retrieved Order Status");
+            Console.WriteLine("Status: {0}", result.Status);
+            Console.WriteLine("Order Id: {0}", result.Id);
+            Console.WriteLine("Intent: {0}", result.Intent);
+            Console.WriteLine("Links:");
+            foreach (LinkDescription link in result.Links)
+            {
+                Console.WriteLine("\t{0}: {1}\tCall Type: {2}", link.Rel, link.Href, link.Method);
+            }
+            AmountWithBreakdown amount = result.PurchaseUnits[0].Amount;
+            Console.WriteLine("Total Amount: {0} {1}", amount.CurrencyCode, amount.Value);
+
+            return response;
+        }
+
+        public static PayPalEnvironment environment()
+        {
+            return new SandboxEnvironment("PAYPAL-SANDBOX-CLIENT-ID", "PAYPAL-SANDBOX-CLIENT-SECRET");
+        }
+
+        /**
+            Returns PayPalHttpClient instance to invoke PayPal APIs.
+         */
+        public static HttpClient client()
+        {
+            return new PayPalHttpClient(environment());
+        }
+
+        public static HttpClient client(string refreshToken)
+        {
+            return new PayPalHttpClient(environment(), refreshToken);
+        }
+
+        /**
+            Use this method to serialize Object to a JSON string.
+        */
+    }
+}
